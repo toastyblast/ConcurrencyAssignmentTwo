0b3aac988177d628b7c0cdb74acc34b9cd3b8d63
Martin <mslavinovslavov@gmail.com>
2017-09-25 21:49:08 +0200

    RopeBridge implementation v1

--- src/RopeBridge.java
+++ src/RopeBridge.java
@@ -5,22 +5,24 @@ public class RopeBridge {
      * your s(hared) data structures to guarantee correct behaviour of the people
      * in passing the rope bridge
      **/
-    private static final int NR_OF_PEOPLE = 20;
+    private static final int NR_OF_PEOPLE = 5;
     private static final int BRIDGE_CAPACITY = 3;
-    private int leftPool = 20;
+    private int leftPool = NR_OF_PEOPLE;
     private int rightPool = 0;
+    private int waitingPeople = 0;
     //false = left
     //true = right
     private boolean leftToRight = false;
+    private Semaphore mutex = new Semaphore(1);
 
     private Person[] person = new Person[NR_OF_PEOPLE];
 
-    private Semaphore leftFreePass, rightFreePass;
+    private Semaphore currentDirection, waitingQueue;
 
     public RopeBridge() {
 
-        leftFreePass = new Semaphore(BRIDGE_CAPACITY, true);
-        rightFreePass = new Semaphore(0, true);
+        currentDirection = new Semaphore(BRIDGE_CAPACITY, true);
+        waitingQueue = new Semaphore(0, true);
 
         for (int i = 0; i < NR_OF_PEOPLE; i++) {
             person[i] = new Person("P" + i); /* argument list can be extended */
@@ -48,45 +50,103 @@ public class RopeBridge {
                 justLive();
                 try {
 
-                    if (leftToRight == direction){
+                    //If this is the direction of the person proceed.
+                    //If this is not the direction of the person, queue on the "waitingPeople line" until the direction is
+                    //changed.
 
-                        leftFreePass.acquire();
-                        System.out.println(getName() + " is on the bridge! " + direction);
+                    if (leftToRight == direction){
+                        //The direction of the person matches that of the bridge.
+                        currentDirection.acquire();
+
+                        //Print from where to where the person is going.
+                        if (!direction){
+                            System.out.println(getName() + " is on the bridge! " + "left to right");
+                        }else if (direction){
+                            System.out.println(getName() + " is on the bridge! " + "right to left");
+                        }
+
 
                         //Wait for a moment...
                         Thread.sleep((2000));
 
                         //Here it all has to happen...
 
-                        leftFreePass.release();
+                        //Move people from one pool to another.
 
                         if (direction){
                             //Update the number of people on each side.
+                            mutex.acquire();
                             rightPool--;
                             leftPool++;
+                            mutex.release();
                         } else if (!direction){
                             //Update the number of people on each side.
+                            mutex.acquire();
                             leftPool--;
                             rightPool++;
+                            mutex.release();
                         }
 
                         //Change your direction
                         direction = !direction;
 
+                        currentDirection.release();
+
                     } else {
-                        rightFreePass.acquire();
+
+                        //This code is executed if the person's direction doesn't mach that of the bridge.
+
+
+                        //Update the number of people that are waiting.
+                        mutex.acquire();
+                        waitingPeople++;
+                        mutex.release();
+
+                        System.out.println(getName() + " has queued." + waitingPeople);
+
+                        //If the number of waitingPeople people is 5(aka everyone has passed the bridge and now are
+                        // waiting for direction change), this means it is time for a change...
+                        if (waitingPeople == 5){
+                            mutex.acquire();
+                            //First, "release" all other people from the waitingPeople queue.
+                            waitingQueue.release(4);
+
+                            //Change the direction depending on the current direction of the bridge.
+                            if (leftPool == 0){
+                                leftToRight = true;
+                            } else if (rightPool == 0){
+                                leftToRight = false;
+                            }
+
+                            //Resets values so the process can be repeated.
+                            //Make a new semaphore so people can wait for the direction to change.
+                            waitingQueue = new Semaphore(0, true);
+                            waitingPeople = 0;
+                            mutex.release();
+
+                        } else {
+
+                            //Wait for the direction to change.
+                            waitingQueue.acquire();
+                            System.out.println(getName() + "stopped waitingPeople");
+                        }
+
                     }
 
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
 
-                if (leftPool == 0){
-                    leftToRight = true;
-                } else if (rightPool == 0){
-                    leftToRight = false;
-                }
-//                System.out.println(getName() + " has passed the bridge!");
+//                try {
+//                    mutex.acquire();
+//                    if (waitingPeople == NR_OF_PEOPLE){
+//                        waitingQueue.release(5);
+//                    }
+//                    mutex.release();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+
             }
         }
 
